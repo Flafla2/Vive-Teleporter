585627d2a31117ad85b1ef1ef40f63ab55dd4b2c
Adrian Biagioli <adrianbiagioli@gmail.com>
2016-05-05 12:07:06 -0400

    Progress on parabolic curve analysis

--- Assets/ParabolicPointer.cs
+++ Assets/ParabolicPointer.cs
@@ -7,7 +7,13 @@ public class ParabolicPointer : MonoBehaviour {
     public Vector3 Acceleration = Vector3.up * -9.8f;
     public int PointCount = 10;
     public float PointSpacing = 0.5f;
-    public float GroundPoint = 
+    public float GroundHeight = 0;
+
+    [NonSerialized]
+    public bool Enabled = false;
+
+    private MeshRenderer Renderer;
+    private MeshFilter Filter;
 
     private static float ParabolicCurve(float p0, float v0, float a, float t)
     {
@@ -37,7 +43,7 @@ public class ParabolicPointer : MonoBehaviour {
 
     private delegate void LineDrawer(Vector3 p1, Vector3 p2);
 
-	private static bool DrawParabolicCurve(Vector3 p0, Vector3 v0, Vector3 a, float dist, int points, Vector3 gndHit, LineDrawer draw)
+	private static bool DrawParabolicCurve(Vector3 p0, Vector3 v0, Vector3 a, float dist, int points, float gnd, out Vector3 gndHit, LineDrawer draw)
     {
         Vector3 last = p0;
         float t = 0;
@@ -45,8 +51,9 @@ public class ParabolicPointer : MonoBehaviour {
         {
             t += dist / ParabolicCurveDeriv(v0, a, t).magnitude;
             Vector3 next = ParabolicCurve(p0, v0, a, t);
-            if (next.y < gndHit.y)
+            if (next.y < gnd)
             {
+                gndHit = Vector3.Lerp(last,next,(gnd - last.y)/(next.y - last.y));
                 draw(last, gndHit);
                 return true;
             } else
@@ -55,15 +62,32 @@ public class ParabolicPointer : MonoBehaviour {
             last = next;
         }
 
-        return true;
+        gndHit = Vector3.zero;
+        return false;
+    }
+
+    void Start() {
+        Renderer = GetComponent<MeshRenderer>();
+        Filter = GetComponent<MeshFilter>();
+
+        if(!Renderer)
+            Renderer = AddComponent<MeshRenderer>();
+        if(!Filter)
+            Filter = AddComponent<MeshFilter>();
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
+        Vector3 hit;
         DrawParabolicCurve(
             transform.position, 
             transform.TransformDirection(InitialVelocity), 
-            Acceleration, PointSpacing, PointCount, Gizmos.DrawLine);
+            Acceleration, PointSpacing, PointCount, 
+            GroundHeight,
+            out hit,
+            Gizmos.DrawLine);
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(hit, 0.2f);
     }
 }
